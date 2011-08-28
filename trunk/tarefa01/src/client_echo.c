5f894d0f2aa4c418d159e06ce2cb4207198ea86c
felipe.aug.silva@gmail.com <felipe.aug.silva@gmail.com@48cb1512-de2e-f615-c636-772228deb8fc>
2011-08-28 23:14:14 +0000

    Inserção de contagem de tempo no cliente, porém é necessário verificar o local correto para capturar o tempo.

--- trunk/tarefa01/src/client_echo.c
+++ trunk/tarefa01/src/client_echo.c
@@ -11,10 +11,16 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <sys/times.h>
+#include <time.h>
 
 #define PORT 3490    /* the port client will be connecting to */
 
 #define MAXDATASIZE 100 /* max number of bytes we can get at once */
+
+/* TODO list
+ * (1) colocar tempo no lugar correto
+ */
 
 int main(int argc, char *argv[])
 {
@@ -22,6 +28,11 @@ int main(int argc, char *argv[])
     char buf[MAXDATASIZE];
     struct hostent *he;
     struct sockaddr_in their_addr; /* connector's address information */
+    clock_t startTime, endTime;
+    struct tms start, end;
+    float elapsedTime;
+    
+    startTime = times(&start); //TODO (1)
 
     if (argc != 2) {
         fprintf(stderr,"usage: client hostname\n");
@@ -39,7 +50,7 @@ int main(int argc, char *argv[])
     }
 
     their_addr.sin_family = AF_INET;         /* host byte order */
-    their_addr.sin_port = htons(PORT);     /* short, network byte order */
+    their_addr.sin_port = htons(PORT);       /* short, network byte order */
     their_addr.sin_addr = *((struct in_addr *)he->h_addr);
     bzero(&(their_addr.sin_zero), 8);        /* zero the rest of the struct */
 
@@ -58,7 +69,11 @@ int main(int argc, char *argv[])
     printf("Received: %s",buf);
 
     close(sockfd);
-
+    
+    endTime = times(&end); //TODO (1)
+    elapsedTime = (float)((endTime - startTime) / sysconf(_SC_CLK_TCK));
+    fprintf(stderr, "Tempo total: %4.1fs\n", elapsedTime);    
+    
     return 0;
 }
 
