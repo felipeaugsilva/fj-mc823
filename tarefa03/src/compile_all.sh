2969520049ae9747a8048d16316ae7ae639383bc
felipe.aug.silva@gmail.com <felipe.aug.silva@gmail.com@48cb1512-de2e-f615-c636-772228deb8fc>
2011-10-04 00:34:55 +0000

    Correção no script de compilação.

--- tarefa03/src/compile_all.sh
+++ tarefa03/src/compile_all.sh
@@ -3,8 +3,8 @@
 echo "Removing old binaries..."
 test -f cludp_echo && rm -f cludp_echo
 test -f srvudp_echo && rm -f srvudp_echo
-echo "Compiling client_echo.sh..."
+echo "Compiling cludp_echo.c..."
 gcc -Wall cludp_echo.c -o cludp_echo
-echo "Compiling server_echo.sh..."
+echo "Compiling srvudp_echo.c..."
 gcc -Wall srvudp_echo.c -o srvudp_echo
 echo "Compilation finished!"
