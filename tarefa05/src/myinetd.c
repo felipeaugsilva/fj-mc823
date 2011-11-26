18c7c0d27ed58d3a903e3ef9575fabcbddd2306c
felipe.aug.silva@gmail.com <felipe.aug.silva@gmail.com@48cb1512-de2e-f615-c636-772228deb8fc>
2011-11-26 14:56:30 +0000

    Parte TCP.

--- tarefa05/src/myinetd.c
+++ tarefa05/src/myinetd.c
@@ -83,7 +83,7 @@ void mysyslog(char *msg)
     fclose(fp);
 }
 
-void printServices(service services[], int total) {  // só pra testes, remover ao final
+void printServices(service services[], int total) {  // TODO: só pra testes, remover ao final
     int i;
     for(i=0; i<total; i++) {
         printf("[%s] ", services[i].name);
@@ -100,12 +100,13 @@ void printServices(service services[], int total) {  // só pra testes, remover 
 int main(int argc, char * argv[])
 {
     service services[MAXSERVICES];
-    char line[MAXLINESIZE];
+    char line[MAXLINESIZE], logMsg[MAXLOGMSG];
     int servIndex = 0;
     int sockfd[MAXSERVICES], new_fd;
     int optval = 1;
-    int i;
     struct sockaddr_in my_addr[MAXSERVICES], their_addr;
+    
+    fprintf( stderr, "Starting myinetd...\n" );
     
     // open configuration file
     char configFileName[] = "myinetd.conf";
@@ -114,52 +115,53 @@ int main(int argc, char * argv[])
     // read services
     while(fgets(line, MAXLINESIZE, configFile) != NULL)
     {
-        strcpy(services[servIndex].name, strtok (line, " "));
-        services[servIndex].port = atoi(strtok (NULL, " "));
+        strcpy(services[ servIndex ].name, strtok (line, " "));
+        services[ servIndex ].port = atoi(strtok (NULL, " "));
         
         if( !strcmp (strtok (NULL, " "), "stream") )
-            services[servIndex].socketType = SOCK_STREAM;
+            services[ servIndex ].socketType = SOCK_STREAM;
         else
-            services[servIndex].socketType = SOCK_DGRAM;
+            services[ servIndex ].socketType = SOCK_DGRAM;
             
-        strcpy(services[servIndex].protoc, strtok (NULL, " "));
-        strcpy(services[servIndex].wait, strtok (NULL, " "));
-        strcpy(services[servIndex].pathname, strtok (NULL, " "));
-        strcpy(services[servIndex].args, strtok (NULL, "\n"));
+        strcpy(services[ servIndex ].protoc, strtok (NULL, " "));
+        strcpy(services[ servIndex ].wait, strtok (NULL, " "));
+        strcpy(services[ servIndex ].pathname, strtok (NULL, " "));
+        strcpy(services[ servIndex ].args, strtok (NULL, "\n"));
         
         servIndex++;
     }
+    fclose( configFile );
     
     // total number of services
     int totalServ = servIndex;
     
     // for all services, create the appropriate socket, bind, and start listening (if tcp)
-    for (i = 0; i < totalServ; i++) 
+    for (servIndex = 0; servIndex < totalServ; servIndex++)
     {
-        my_addr[i].sin_family = AF_INET;                 /* host byte order */
-        my_addr[i].sin_port = htons(services[i].port);   /* short, network byte order */
-        my_addr[i].sin_addr.s_addr = INADDR_ANY;         /* automatically fill with my IP */
-        bzero(&(my_addr[i].sin_zero), 8);                /* zero the rest of the struct */
+        my_addr[ servIndex ].sin_family = AF_INET;                           /* host byte order */
+        my_addr[ servIndex ].sin_port = htons(services[ servIndex ].port);   /* short, network byte order */
+        my_addr[ servIndex ].sin_addr.s_addr = INADDR_ANY;                   /* automatically fill with my IP */
+        bzero(&(my_addr[ servIndex ].sin_zero), 8);                          /* zero the rest of the struct */
         
-        if ((sockfd[i] = socket(AF_INET, services[i].socketType, 0)) == -1) {
+        if ((sockfd[ servIndex ] = socket(AF_INET, services[ servIndex ].socketType, 0)) == -1) {
             perror("socket");
             exit(1);
         }   
             
         // lose the pesky "address already in use" error message
-        if (setsockopt(sockfd[i], SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) == -1) {
+        if (setsockopt(sockfd[ servIndex ], SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) == -1) {
             perror("setsockopt");
             exit(1);
         }
         
-        if (bind(sockfd[i], (struct sockaddr *)&my_addr[i], sizeof(struct sockaddr)) == -1) {
+        if (bind(sockfd[ servIndex ], (struct sockaddr *)&my_addr[ servIndex ], sizeof(struct sockaddr)) == -1) {
             perror("bind");
             exit(1);
         }
         
         // tcp socket, start listening
-        if ( ! strcmp(services[i].protoc, "tcp") ) {
-            if (listen(sockfd[i], BACKLOG) == -1) {
+        if ( ! strcmp(services[ servIndex ].protoc, "tcp") ) {
+            if (listen(sockfd[ servIndex ], BACKLOG) == -1) {
                 perror("listen");
                 exit(1);
             }
@@ -169,11 +171,14 @@ int main(int argc, char * argv[])
     fd_set fds;
     FD_ZERO(&fds);
     
+    //daemon_init( argv[0] );          /* install server as a daemon */
+    //mysyslog( "myinetd started...\n" );
+    
     /* main loop */
     while(1)
     {
-        for (i = 0; i < totalServ; i++) {    //TODO: setar todos sempre???
-            FD_SET(sockfd[i], &fds);
+        for (servIndex = 0; servIndex < totalServ; servIndex++) {    //TODO: setar todos sempre???
+            FD_SET(sockfd[ servIndex ], &fds);
         }
         
         if (select(sockfd[totalServ-1]+1, &fds, NULL, NULL, NULL) < 0) {
@@ -182,26 +187,44 @@ int main(int argc, char * argv[])
         }
         
         // iterate through all the services to check which one was called
-        for (i = 0; i < totalServ; i++)
+        for (servIndex = 0; servIndex < totalServ; servIndex++)
         {
-            if (FD_ISSET(sockfd[i], &fds))
+            if ( FD_ISSET(sockfd[ servIndex ], &fds) )
             {
-                if ( ! strcmp(services[i].protoc, "tcp") )      // TCP socket
+                // TCP socket
+                if ( ! strcmp(services[ servIndex ].protoc, "tcp") )
                 {
+                    fprintf( stderr, "got tcp...\n" ); // TODO: remover
+                    
                     unsigned int sin_size = sizeof(struct sockaddr_in);
                     
-                    if ((new_fd = accept(sockfd[i], (struct sockaddr *)&their_addr, &sin_size)) == -1) {
+                    if ((new_fd = accept(sockfd[ servIndex ], (struct sockaddr *)&their_addr, &sin_size)) == -1) {
                         perror("accept");
                         exit(1);
                     }
                     if ( ! fork() )
                     {
-                        // TODO: trata servico TCP
+                        // close all sockets other than new_fd
+                        int i;
+                        for (i = 0; i < totalServ; i++)
+                            close( sockfd[ i ] );
+                        
+                        dup2( fileno(fdopen(new_fd, "r")), 0 );
+                        dup2( fileno(fdopen(new_fd, "w")), 1 );
+                        dup2( fileno(fdopen(new_fd, "w")), 2 );
+                        
+                        execv( services[ servIndex ].name, services[ servIndex ].args );
+                        
+                        close( new_fd );
+                        exit( 0 );
                     }
                     close( new_fd );
                 }
-                else    // TODO: UDP socket
+                // TODO: UDP socket
+                else
                 {
+                    fprintf( stderr, "got udp...\n" ); // TODO: remover
+                    
                     if ( ! fork() )
                     {
                         //trata servico UDP
