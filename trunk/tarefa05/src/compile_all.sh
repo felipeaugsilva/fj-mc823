69bbdf2c31c2735a535c29073b291f387d7c68fb
felipe.aug.silva@gmail.com <felipe.aug.silva@gmail.com@48cb1512-de2e-f615-c636-772228deb8fc>
2011-11-29 20:26:06 +0000

    Log do servidor de eco.

--- trunk/tarefa05/src/compile_all.sh
+++ trunk/tarefa05/src/compile_all.sh
@@ -4,8 +4,11 @@ echo "Removing old binaries..."
 test -f myinetd && rm -f myinetd
 test -f mydaytime_tcp && rm -f mydaytime_tcp
 test -f mydaytime_udp && rm -f mydaytime_udp
+test -f server_echo && rm -f server_echo
 echo "Compiling myinetd.c ..."
 gcc -g -Wall myinetd.c -o myinetd
+echo "Compiling server_echo.c ..."
+gcc -g -Wall server_echo.c -o server_echo
 echo "Compiling mydaytime_tcp.c ..."
 gcc -g -Wall mydaytime_tcp.c -o mydaytime_tcp
 echo "Compiling mydaytime_udp.c ..."
--- trunk/tarefa05/src/server_echo.c
+++ trunk/tarefa05/src/server_echo.c
@@ -9,13 +9,42 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define MAXDATASIZE 1000   /* max number of bytes we can get at once */
+
+
+/* appends message to log file */
+void mysyslog(char *msg)
+{
+    FILE * fp;
+    char buf[64], * ctime();
+    time_t time(), now;
+    (void) time(&now);               /* get seconds since Epoch into now */
+    
+    sprintf(buf, "%s", ctime(&now)); /* change secs to date and time, put in buf */
+    
+    if ((fp= fopen("server_echo.log", "a")) == 0)   /* private log file */
+        exit(1);
+    
+    fprintf(fp, "%s%s\n\n", buf, msg);
+    fclose(fp);
+}
 
 
 int main()
 {
     char buffer[ MAXDATASIZE ];
+    struct sockaddr_in their_addr;
+    
+    unsigned int sin_size = sizeof(struct sockaddr_in);
+    
+    getpeername( 0, (struct sockaddr *)&their_addr, &sin_size );
+    
+    sprintf( buffer, "Client: %s:%d", inet_ntoa(their_addr.sin_addr), ntohs(their_addr.sin_port) );
+    mysyslog( buffer );
     
     while ( fgets( buffer, MAXDATASIZE, stdin ) != NULL )
     {
